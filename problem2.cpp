b080035f64462f556b88668314643d7b1623e4d5
maxengel99 <maxengel99@gmail.com>
2016-11-29 20:16:08 -0600

    Add problem 3 and update problem 2

--- problem2.cpp
+++ problem2.cpp
@@ -1,1 +1,3 @@
-
+#include <iostream>
+#include <fstream>
+using namespace std;int main(){ifstream b("a.txt");string a="";while(b.good()){string c;getline(b,c);c+="\n";a+=c;}cout<<a<<endl;}
---
+++ problem3.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+int main(){std::cout<<'a'-'`'<<" ("<<(int)'='<<'A'-'<'<<") "<<'~'+'~'+'F'<<"-"<<(int)'I'<<'V'-'K';}
